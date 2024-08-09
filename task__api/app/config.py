4dd4fd642b6fbc0cd08856e53f68e13ed4cd66cf
Oguz Can Varol <oguzcnvaroll@gmail.com>
2024-08-09 12:27:56 +0300

    son

--- task__api/app/config.py
+++ task__api/app/config.py
@@ -1,8 +1,8 @@
 import os
-import urllib.parse
+from dotenv import load_dotenv
 
-original_url = os.getenv('DATABASE_URL')
-if original_url:
-    parsed = urllib.parse.urlparse(original_url)
-    formatted_url = urllib.parse.urlunparse(parsed._replace(netloc=urllib.parse.quote(parsed.username) + ':' + urllib.parse.quote(parsed.password) + '@' + parsed.hostname + (':' + str(parsed.port) if parsed.port else '')))
-    app.config['SQLALCHEMY_DATABASE_URI'] = formatted_url.replace("mysql://", "mysql+pymysql://", 1)
+load_dotenv()  
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'DATABASE_URL'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
